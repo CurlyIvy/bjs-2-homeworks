d247ed3dccc038c0dffa868b5321bca72443ea46
CurlyIvy <namsaraevvladimir89@gmail.com>
2021-07-01 13:12:53 +0300

    homework #3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,36 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+function compareArrays(firstArr, secondArr) {
+  let result = false;
+  // validate args
+  if(!Array.isArray(firstArr) || !Array.isArray(secondArr)) {
+    return result;
+  }
+  if(firstArr.length !== secondArr.length) {
+    return result;
+  }
+  // compare arrays
+  result = firstArr.every((item, index) => { 
+    if(index >= secondArr.length) {
+      return false;
+    }
+    let result = false;
+    if(item === secondArr[index]) {
+      result = true;
+    }
+    return result;
+  });
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((item) => {
+    // positive numbers filter
+    return item > 0;
+  }).filter((item) => {
+    // even three filter
+    return item % 3 === 0;
+  }).map((item) => {
+    // multiply by number
+    return item * 10;
+  });
+  return resultArr;
 }
