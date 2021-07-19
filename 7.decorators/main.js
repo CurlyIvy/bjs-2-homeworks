54526af60b59d8b38b05b6947852e58b63468b2f
CurlyIvy <namsaraevvladimir89@gmail.com>
2021-07-19 03:08:29 +0300

    homework #7

--- 7.decorators/main.js
+++ 7.decorators/main.js
@@ -9,12 +9,23 @@ upgradedAddThree(5, 2, 3);
 upgradedAddThree(6, 2, 3); 
 upgradedAddThree(1, 2, 3); 
 
-const sendSignal = () => console.log('Сигнал послан'); 
-const upgradedSendSignal = debounceDecoratorNew(sendSignal,2000);
-setTimeout(upgradedSendSignal()); 
-setTimeout(upgradedSendSignal(),300); 
-setTimeout(upgradedSendSignal(),900); 
-setTimeout(upgradedSendSignal(),1200);
-setTimeout(upgradedSendSignal(),2300);
-setTimeout(upgradedSendSignal(),4400);
-setTimeout(upgradedSendSignal(),4500);
+const sendSignalFirst = () => console.log('Сигнал послан'); 
+const upgradedSendSignalFirst = debounceDecoratorNew(sendSignalFirst,2000);
+setTimeout(upgradedSendSignalFirst); 
+setTimeout(upgradedSendSignalFirst,300); 
+setTimeout(upgradedSendSignalFirst,900); 
+setTimeout(upgradedSendSignalFirst,1200);
+setTimeout(upgradedSendSignalFirst,2300);
+setTimeout(upgradedSendSignalFirst,4400);
+setTimeout(upgradedSendSignalFirst,4500);
+
+const sendSignalSecond = () => console.log('Сигнал послан'); 
+const upgradedSendSignalSecond = debounceDecorator2(sendSignalSecond,2000);
+setTimeout(upgradedSendSignalSecond); 
+setTimeout(upgradedSendSignalSecond,300); 
+setTimeout(upgradedSendSignalSecond,900); 
+setTimeout(upgradedSendSignalSecond,1200);
+setTimeout(upgradedSendSignalSecond,2300);
+setTimeout(upgradedSendSignalSecond,4400);
+setTimeout(upgradedSendSignalSecond,4500);
+
