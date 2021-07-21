31de19c9a0d0cac4ea9e816a66420226406ffa97
CurlyIvy <namsaraevvladimir89@gmail.com>
2021-07-21 03:16:33 +0300

    homework #8

--- 8.async/main.js
+++ 8.async/main.js
@@ -1,1 +1,31 @@
-// тут вы можете вызывать функции из task.js
+let alarmClock = new AlarmClock();      
+let forrmat = DateForrmatHelper.toString(); 
+let func = multyAction;
+alarmClock.addClock(forrmat, func, getRandomInt(1000));
+alarmClock.addClock(forrmat, test, getRandomInt(1000));
+//alarmClock.addClock(forrmat, () => (console.log(this)), getRandomInt(1000));
+alarmClock.start();
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function singleAction() {
+    console.log(singleAction.name);
+    console.log('all alarms befor:');
+    this.printAlarms();
+    this.clearAlarms();
+    console.log('all alarms after:');
+    this.printAlarms();
+}
+
+function multyAction() {
+    console.log(multyAction.name);
+    console.log('all alarms:');
+    this.printAlarms();   
+}
+
+function test() {
+    console.log(test.name);  
+}
